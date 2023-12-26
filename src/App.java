5ef5f789710a2771dbcf9392057a70b518d986f7
Paulo Negrelli <paulo.anhaia7@gmail.com>
2023-12-26 15:26:32 -0300

    First class: Position

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import entities.boardgame.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Chess System.");
     }
 }
