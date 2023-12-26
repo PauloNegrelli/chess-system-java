5dc57e093b9159cb8322ee88f1168c91776fafe3
Paulo Negrelli <paulo.anhaia7@gmail.com>
2023-12-26 15:36:22 -0300

    Starting to implement Board and Piece.

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
-import entities.boardgame.Position;
+import entities.boardgame.Board;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Chess System.");
+
+        Board board = new Board(8, 8);
     }
 }
